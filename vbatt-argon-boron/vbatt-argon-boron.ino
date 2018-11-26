3d47de647d38a6edf9255b69bee2423afe0929d8
Kenneth Lim <kennethlimcp@gmail.com>
2018-11-26 20:25:01 +0800

    fix: R2 resistor value
    Signed-off-by: Kenneth Lim <kennethlimcp@gmail.com>

--- vbatt-argon-boron/vbatt-argon-boron.ino
+++ vbatt-argon-boron/vbatt-argon-boron.ino
@@ -2,20 +2,24 @@
 Written by kennethlimcp on 25 November 2018
 Argon and Xenon battery voltage calculation
 
+Official docs: https://docs.particle.io/reference/device-os/firmware#battery-voltage
+
 GND ---- R2 ---- R1 --- Vbatt
              |
              |
             Vbatt_adc (P0.05, BATT)
 
 R1 =  806 kOhm
-R2 = 2100 kOhm
-
-Vbatt_adc = Vbatt * R2/(R1 + R2) 
-Vbatt = Vbatt_adc * (R1 + R2)/R2
-Vbatt = Vbatt_adc * (2100 + 806)/2100
-Vbatt = Vbatt_adc * 1.383809523809524
+R2 = 2000 kOhm
 
 ADC resolution is 3.3 volts / 4096 units or, 0.0008 volts (0.8 mV) per unit
+
+Vbatt_adc = Vbatt * R2/(R1 + R2) * 0.0008
+Vbatt = Vbatt_adc * (R1 + R2)/R2 * 0.0008
+Vbatt = Vbatt_adc * (2000 + 806)/2000 * 0.0008
+Vbatt = Vbatt_adc * 1.403 * 0.0008
+Vbatt = Vbatt_adc * 0.0011224
+
 */
 
 void setup() {
@@ -26,7 +30,7 @@ unsigned long old_time = 0;
 
 void loop() {
     if(millis() - old_time > 10000) {
-        float voltage = analogRead(BATT) * 0.0008 * 1.3838;
+        float voltage = analogRead(BATT) * 0.0011224;
         
         Particle.publish("voltage", String::format("%.2f",voltage), PRIVATE);
         old_time = millis();
